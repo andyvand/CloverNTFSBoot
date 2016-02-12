81b6be99650c9425922d177b0f408f34e0d32e19
andyvand <andyvand>
2016-02-12 08:53:28 +0100

    Added new alternate booting PBR and readded old bootsector

--- build.bat
+++ build.bat
@@ -1,7 +1,10 @@
 set PATH=C:\WATCOM\BINNT;%PATH%
 ml /c /Cx /omf /AT /Zm /Fo"ntfsboot.obj" ntfsboot.asm
 ml /c /Cx /omf /AT /Zm /Fo"ntfsbootalt.obj" ntfsbootalt.asm
+ml /c /Cx /omf /AT /Zm /Fo"ntfsbootaltbmgr.obj" ntfsbootaltbmgr.asm
 wlink name ntfsboot d all sys dos com op m op maxe=25 @ntfsboot.lk1
 wlink name ntfsbootalt d all sys dos com op m op maxe=25 @ntfsbootalt.lk1
+wlink name ntfsbootaltbmgr d all sys dos com op m op maxe=25 @ntfsbootaltbmgr.lk1
 dd if=ntfsboot.com of=ntfsboot.bin count=1 bs=8192
 dd if=ntfsbootalt.com of=ntfsbootalt.bin count=1 bs=8192
+dd if=ntfsbootaltbmgr.com of=ntfsbootaltbmgr.bin count=1 bs=8192
